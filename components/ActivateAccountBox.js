9a805036e20014f66786c936e41742316a0cc62e
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2018-09-18 16:48:24 +0100

    Validate the account using an href link

--- components/ActivateAccountBox.js
+++ components/ActivateAccountBox.js
@@ -1,18 +1,18 @@
+import React from "react";
 import { Mutation, withApollo } from "react-apollo";
 import gql from "graphql-tag";
 import cookie from "cookie";
 import { graphql } from "react-apollo";
 import redirect from "../lib/redirect";
 
-// const ActivateAccountBox = props => {
-//   return (
-//     <div>
-//       <h1>{console.log(props)}</h1>
-//     </div>
-//   );
-// };
-
-// export default withApollo(ActivateAccountBox);
+// Mutation : Activate the Account
+const ACTIVATE_ACCOUNT = gql`
+  mutation ActivateAccount($activationCode: ID!) {
+    activateAccount(id: $activationCode) {
+      result
+    }
+  }
+`;
 
 class ActivateAccountBox extends React.Component {
   constructor(props) {
@@ -21,32 +21,27 @@ class ActivateAccountBox extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>{console.log(this.props.data)}</h1>
-      </div>
+      <Mutation mutation={ACTIVATE_ACCOUNT}>
+        {(activateAccount, { loading, error }) => (
+          <div>
+            <a
+              href="#"
+              onClick={e => {
+                e.preventDefault();
+                activateAccount({
+                  variables: { activationCode: this.props.activationCode },
+                });
+              }}
+            >
+              Validate
+            </a>
+            {loading && <p>Loading...</p>}
+            {error && <p>Error :( Please try again</p>}
+          </div>
+        )}
+      </Mutation>
     );
   }
 }
 
-const ACTIVATE_ACCOUNT = gql`
-  query AccountActivationCode($activationId: ID) {
-    AccountActivationCode(id: $activationId) {
-      user {
-        name
-      }
-    }
-  }
-`;
-
-export default graphql(ACTIVATE_ACCOUNT, {
-  props: ({ data }) => ({
-    data,
-  }),
-  options(ownProps) {
-    return {
-      variables: {
-        activationId: "cjm6rfntu15dz0117oaznx76s",
-      },
-    };
-  },
-})(withApollo(ActivateAccountBox));
+export default withApollo(ActivateAccountBox);
